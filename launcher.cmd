fa4bfb1bd2bd6495853dffb84d2691f347e52d31
Denis Kazakov <denis.kazakoff@gmail.com>
2021-11-23 16:51:04 +0300

    Add files via upload

--- launcher.cmd
+++ launcher.cmd
@@ -2,6 +2,6 @@
 %~d0
 cd %~dp0
 SET PYTHONPATH=%cd%\python
-SET PATH=%PATH%%cd%\python;%cd%\python\Scripts;
-START pythonw.exe main.py
+SET PATH=%PATH%;%cd%\python;%cd%\python\Scripts;
+start pythonw.exe main.py
 REM PAUSE
